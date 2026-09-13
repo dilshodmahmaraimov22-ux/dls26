22c782226a779131851705e535854fae377d6630
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-13 16:18:05 +0500

    link qoshildi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import dls from '../images/dls.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -8,7 +9,11 @@ const Header = () => {
         <div className='container'>
             <div className='header__container'>
                 <img src={dls} alt="rasm" />
-                <ul className=''></ul>
+                <ul className='header__list'>
+                    <li className='header__item'><Link to="/">Home</Link></li>
+                    <li className='header__item'><Link to="/service">Service</Link></li>
+                    <li className='header-item'><Link to="/akk">Akk</Link></li>
+                </ul>
             </div>
         </div>
     </div>
