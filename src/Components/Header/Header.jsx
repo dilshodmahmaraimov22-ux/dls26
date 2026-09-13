8dfc1810994dacdeb8d54bdd3cb91a60cb0591af
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-13 15:52:57 +0500

    rasm qoshildi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import logo from "./Images/dls.png"
 
 const Header = () => {
   return (
     <>
+    <div className='header'>
+        <div className='container'>
+            <div className='header__container'>
+                <img src={logo} alt="" />
+            </div>
+        </div>
+    </div>
     </>
   )
 }
