7dc3e98a0672be91c798410234331d5f433a9c5b
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-13 18:57:32 +0500

    css tog'irlandi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import dls from '../Images/dls.png';
-import './header.css';
+import './Header.css';
 
 const Header = () => {
   return (
