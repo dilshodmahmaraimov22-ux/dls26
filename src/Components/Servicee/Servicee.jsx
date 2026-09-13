3d73c807a2506b6a4fb0e7f27d50e54d62131038
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-13 18:05:43 +0500

    service tugadi

--- src/Components/Servicee/Servicee.jsx
+++ src/Components/Servicee/Servicee.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import tanga from '../images/tanga.jpg'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Servicee.css';
+import tanga from '../images/tanga.jpg';
+import { Link } from 'react-router-dom';
 
 const Servicee = () => {
   return (
-    <>
-    <div className='header'>
-        <div className='container'>
-            <div className='header-container'>
-                <h1 className='header-title'>TANGA VA OLMOS YIG'ISH NARXI</h1>
-                <img className='header-img' src={tanga} alt="rasm" />
-                <p className='header-text'>ASSALOMU ALAYKUM DLS26 ISHQIBOZIKECHA KELGAN LEGENDLAR PETIT, COLE, ESSIEN, BERBATOV Larni qanday olishni bilmayapsizmi? Unda buning yechimi oson. Yo'q menga bular kerak emas deyszimi? Unda atiga bir necha kunlardan keyin keladigan CULT HEROES GA nima deysiz? Tezda bizning tanga olmos xizmatimizdan foydalaning va akkauntingizni kuchaytiring.</p>
-                <Link className='header-link' to="https://t.me/dream0884" target='blank'>Bizga bog'lanish</Link>
-            </div>
-        </div>
-    </div>
-    </>
-  )
-}
+    <section className="service-page">
+      <div className="service-container">
+        <h1 className="service-title">TANGA VA OLMOS YIG'ISH NARXI</h1>
 
-export default Servicee
+        <div className="service-img-wrapper">
+          <img className="service-img" src={tanga} alt="DLS Tanga va Olmos xizmati" />
+        </div>
+
+        <p className="service-text">
+          <strong>ASSALOMU ALAYKUM DLS26 ISHQIBOZI!</strong> Kecha kelgan legendlar Petit, Cole, Essien, 
+          Berbatovlarni qanday olishni bilmayapsizmi? Unda buning yechimi oson. Yo'q menga bular kerak emas deysizmi? 
+          Unda atiga bir necha kunlardan keyin keladigan <strong>CULT HEROES</strong> ga nima deysiz? 
+          Tezda bizning tanga va olmos xizmatimizdan foydalaning va akkauntingizni kuchaytiring.
+        </p>
+
+        <Link 
+          className="service-btn" 
+          to="https://t.me/dream0884" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          Bizga bog'lanish
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Servicee;
